aa5bdff98ab2566e02aaa6227e60d78cfb4b8700
Stefan-ci <claverdiby9@gmail.com>
2022-03-16 19:45:07 +0000

    Sitemap stuffs

---
+++ soap/sitemaps.py
@@ -0,0 +1,43 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class HomeSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home', 'item-list', 'item-tag', 'all-items', 'men-items',
+                    'women-items', 'children-items', 'search-items']
+
+    def location(self, item):
+        return reverse(item)
+
+
+
+class StaticSitemap(Sitemap):
+    priority = 0.9
+    changefreq = 'daily'
+
+    def items(self):
+        return [
+            'login',
+            'register',
+            'user-cart',
+            'checkout',
+            'confirm-order',
+            'add-coupon',
+            'request-refund',
+            'favourites',
+            'country-cities',
+            
+            'about',
+            'contact',
+            'support',
+            'reset_password',
+            'password_reset_done',
+            'password_reset_complete',
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- soap/urls.py
+++ soap/urls.py
@@ -1,3 +1,4 @@
+from products.models import Soap
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
@@ -6,7 +7,11 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic.base import TemplateView
 from django.contrib.sitemaps import GenericSitemap
+from soap.sitemaps import HomeSitemap, StaticSitemap
 
+items_dict = {
+    'queryset': Soap.objects.filter(is_public=True, is_deleted=False)
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,6 +24,17 @@ urlpatterns = [
     path('robots.txt/', TemplateView.as_view(
         template_name='robots.txt',
         content_type='text/plain')),
+    
+    
+    path('sitemap.xml/', sitemap,
+        {
+            'sitemaps': {
+                'home': HomeSitemap,
+                'static': StaticSitemap,
+                'item-detail': GenericSitemap(items_dict, priority=1.0),
+            },
+        },
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, serve, document_root=settings.STATIC_ROOT)
